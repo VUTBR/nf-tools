c11e826f11f6dae2219d7fc18974d541a41fb4c9
tpoder@cis.vutbr.cz <tpoder1@users.noreply.github.com>
2012-11-30 10:34:31 +0000

    libnf for perl (Net-NfDump module), initial commit

---
+++ libnf/perl/Net-NfDump/lib/Net/NfDump.pm
@@ -0,0 +1,295 @@
+package Net::NfDump;
+
+use 5.010001;
+use strict;
+use warnings;
+use Carp;
+
+require Exporter;
+use AutoLoader;
+
+our @ISA = qw(Exporter);
+
+# Items to export into callers namespace by default. Note: do not export
+# names by default without a very good reason. Use EXPORT_OK instead.
+# Do not simply export all your public functions/methods/constants.
+
+# This allows declaration	use Net::NfDump ':all';
+# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
+# will save memory.
+our %EXPORT_TAGS = ( 'all' => [ qw(
+	
+) ] );
+
+our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+
+our @EXPORT = qw(
+	
+);
+
+our $VERSION = '0.01';
+
+sub AUTOLOAD {
+    # This AUTOLOAD is used to 'autoload' constants from the constant()
+    # XS function.
+
+    my $constname;
+    our $AUTOLOAD;
+    ($constname = $AUTOLOAD) =~ s/.*:://;
+    croak "&Net::NfDump::constant not defined" if $constname eq 'constant';
+    my ($error, $val) = constant($constname);
+    if ($error) { croak $error; }
+    {
+	no strict 'refs';
+	# Fixed between 5.005_53 and 5.005_61
+#XXX	if ($] >= 5.00561) {
+#XXX	    *$AUTOLOAD = sub () { $val };
+#XXX	}
+#XXX	else {
+	    *$AUTOLOAD = sub { $val };
+#XXX	}
+    }
+    goto &$AUTOLOAD;
+}
+
+require XSLoader;
+XSLoader::load('Net::NfDump', $VERSION);
+
+# Preloaded methods go here.
+
+# Autoload methods go after =cut, and are processed by the autosplit program.
+
+# Below is stub documentation for your module. You'd better edit it!
+
+=head1 NAME
+
+Net::NfDump - Perl extension for blah blah blah
+
+=head1 SYNOPSIS
+
+  use Net::NfDump;
+  blah blah blah
+
+=head1 DESCRIPTION
+
+Stub documentation for Net::NfDump, created by h2xs. It looks like the
+author of the extension was negligent enough to leave the stub
+unedited.
+
+Blah blah blah.
+
+=head2 EXPORT
+
+=head1 METHODS
+
+=cut 
+
+# merge $opts with class default opts and return the resilt. 
+
+sub merge_opts {
+	my ($self, %opts) = @_;
+
+	my $ropts = {};
+	while ( my ($key, $val) =  each %{$self->{opts}} ) {
+		$ropts->{$key} = $val;
+
+	}
+
+	while ( my ($key, $val) =  each %opts ) {
+		$ropts->{$key} = $val;
+	}
+
+	return $ropts; 
+}
+
+
+=head2 new
+The constructor. As the parameter options can be specified. This options will be used
+as a default option set in the particular methods. 
+
+=cut
+
+sub new {
+	my ($self, %opts) = @_;
+
+	my ($class) = {};
+	bless($class);
+
+	my $handle = Net::NfDump::libnf_init();
+
+	if (!defined($handle) || $handle == 0) {
+		return undef;
+	} 
+
+	$class->{opts} = { 
+		InputFiles => [],
+		Filter => "any",
+		TimeWindowStart => 0,
+		TimeWindowEnd => 0,
+		OutputFile => undef,
+		Compressed => 1,
+		Anonymized => 1,
+		Ident => ""
+	};
+
+	$class->{opts} = $class->merge_opts(%opts);
+		
+	$class->{handle} = $handle;	
+
+	$class->{read_prepared} = 0;
+	$class->{write_prepared} = 0;
+
+	return $class;
+}
+
+=head2 query
+Query method can be used in two ways. If the string argument is the 
+flow query is handled. See section FLOW QUERY how to create flow 
+queries.
+
+=cut
+
+sub query {
+	my ($self, %opts) = @_;
+
+	my $o = $self->merge_opts(%opts);
+
+	if (@{$o->{InputFiles}} == 0) {
+		croak("No imput files defined");
+	} 
+
+	# handle, filter, windows start, windows end, ref to filelist 
+	Net::NfDump::libnf_read_files($self->{handle}, $o->{Filter}, 
+					$o->{TimeWindowStart}, $o->{TimeWindowEnd}, 
+					$o->{InputFiles});	
+
+	$self->{read_prepared} = 1;
+
+}
+
+=head2 read
+Have to be used after query method. If the query wasn't called before the 
+method is called as $obj->query() before the first record is returned. 
+
+Method returns hash reference with the record and skips to the next record. Returns 
+true if there are more records to read or false if all record from all 
+files have been read. 
+
+=cut
+
+sub read {
+	my ($self) = @_;
+
+	if (!$self->{read_prepared}) {
+		$self->query();
+	}
+
+	my $ret = Net::NfDump::libnf_read_row($self->{handle});
+
+	#the end of the file/files we set back read prepared to 0
+	if (!$ret) {
+		$self->{read_prepared} = 0;
+	}
+
+	return $ret;
+}
+
+=head2 create_file
+Creates a new nfdump file. 
+
+=cut 
+sub create {
+	my ($self, %opts) = @_;
+
+	my $o = $self->merge_opts(%opts);
+
+	if (!defined($o->{OutputFile}) || $o->{OutputFile} eq "") {
+		croak("No output file defined");
+	} 
+
+	# handle, filename, compressed, anonyized, identifier 
+	Net::NfDump::libnf_create_file($self->{handle}, 
+		$o->{OutputFile}, 
+		$o->{Compressed},
+		$o->{Anonymized},
+		$o->{Ident});
+
+	$self->{write_prepared} = 1;
+}
+
+
+=head2 insert
+Insert data defined in hashref to the file opened by create. 
+
+=cut
+
+sub write {
+	my ($self, $row) = @_;
+
+	if (!$self->{write_prepared}) {
+		$self->create();
+	}
+	
+	# handle, row reference
+	return Net::NfDump::libnf_write_row($self->{handle}, $row);
+}
+
+sub close {
+	my ($self) = @_;
+
+	# handle, row reference
+	Net::NfDump::libnf_finish($self->{handle});
+
+	$self->{read_prepared} = 0;
+	$self->{write_prepared} = 0;
+}
+
+sub DESTROY {
+	my ($self) = @_;
+
+	# handle, row reference
+	Net::NfDump::libnf_finish($self->{handle});
+}
+
+
+=head1 FLOW QUERY
+The flow query is language vyry simmilar to SQL to query data on 
+nfdump files. However flow query have nothing to do with SQL. It uses
+only simmilar command syntax. Example of flow query 
+
+SELECT * FROM data/nfdump1.nfcap, data2/nfdump2.nfcap
+WHERE src host 147.229.3.10 
+TIME WINDOW BETWEEN '2012-06-03' AND '202-06-04' 
+ORDER BY bytes
+LIMIT 100
+
+=head1 SEE ALSO
+
+Mention other useful documentation such as the documentation of
+related modules or operating system documentation (such as man pages
+in UNIX), or any relevant external documentation such as RFCs or
+standards.
+
+If you have a mailing list set up for your module, mention it here.
+
+If you have a web site set up for your module, mention it here.
+
+=head1 AUTHOR
+
+root, E<lt>root@localdomainE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2012 by root
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10.1 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
+
+1;
+
+__END__
+
---
+++ libnf/perl/Net-NfDump/libnf/bit_array.h
@@ -0,0 +1,67 @@
+
+
+/* bit operations  */
+typedef struct bit_array_s {
+	int size;
+	char *data;
+#define BA_DATA_LEN 8
+} bit_array_t;
+
+
+inline bit_array_t * bit_array_init(bit_array_t *a, int size) {
+
+	int bytes = size / BA_DATA_LEN + 1;
+
+	a->size = size;
+	a->data = malloc(bytes);
+
+	if (a->data != NULL) {
+		memzero(a->data, bytes);
+		return a;
+	} else 
+		return NULL;	
+}
+
+inline int bit_array_get(bit_array_t *a, int pos) {
+	if (pos >= a->size) 
+		return -1;
+    return a->data[pos / BA_DATA_LEN] & (1 << (pos % BA_DATA_LEN));
+}
+
+inline int bit_array_set(bit_array_t *a, int pos, int val) {
+	if (pos > a->size) 
+		return -1;
+
+	if (val) 
+		a->data[ pos / BA_DATA_LEN ] |= 1 << (pos % BA_DATA_LEN );
+	 else 
+		a->data[ pos / BA_DATA_LEN ] &= ~ (1 << (pos % BA_DATA_LEN ));
+
+	return 0;
+}
+
+
+inline int bit_array_cmp(bit_array_t *a, bit_array_t *b) {
+
+	int bytes = a->size / BA_DATA_LEN + 1;
+
+	/* can can compare only arrays with same sizes */
+	if (a->size != b->size) 
+		return -1;
+
+	return memcmp(a->data, b->data, bytes);
+}
+
+inline int bit_array_copy(bit_array_t *d, bit_array_t *s) {
+	
+	int bytes = s->size / BA_DATA_LEN + 1;
+
+	/* we can copy only arrays with same sizes */
+	if (s->size != d->size) 
+		return -1;
+
+	return (memcpy(&d->data, &s->data, bytes) != NULL);
+
+}
+
+
