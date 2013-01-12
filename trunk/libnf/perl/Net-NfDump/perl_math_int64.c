0c77921f6b5d8b93b6a7d15ac43f71b02f9721b5
tpoder@cis.vutbr.cz <tpoder@cis.vutbr.cz@6e27440e-4b98-2107-0108-1f70ee4755a6>
2013-01-12 18:47:53 +0000

    Math-Int64 changed to v1 compatible

--- trunk/libnf/perl/Net-NfDump/perl_math_int64.c
+++ trunk/libnf/perl/Net-NfDump/perl_math_int64.c
@@ -1,10 +1,8 @@
 /*
- * perl_math_int64.c - This file is in the public domain
- * Author: Salvador Fandino <sfandino@yahoo.com>
+ * perl_math_int64.h - This file is in the public domain
  *
- * Generated on: 2013-01-07 20:05:31
- * Math::Int64 version: 0.29
- * Module::CAPIMaker version: 0.02
+ * Author: Salvador Fandino <sfandino@yahoo.com>
+ * Version: 1.2
  */
 
 #include "EXTERN.h"
@@ -24,111 +22,40 @@ typedef unsigned __int64 uint64_t;
 /* you may need to add a typemap for int64_t here if it is not defined
    by default in your C header files */
 
-HV *math_int64_c_api_hash;
-int math_int64_c_api_min_version;
-int math_int64_c_api_max_version;
+HV *math_int64_capi_hash;
+int math_int64_capi_version;
+SV *(*math_int64_capi_newSVi64)(pTHX_ int64_t);
+SV *(*math_int64_capi_newSVu64)(pTHX_ uint64_t);
+int64_t (*math_int64_capi_SvI64)(pTHX_ SV*);
+uint64_t (*math_int64_capi_SvU64)(pTHX_ SV*);
+int (*math_int64_capi_SvI64OK)(pTHX_ SV*);
+int (*math_int64_capi_SvU64OK)(pTHX_ SV*);
 
-int64_t   (*math_int64_c_api_SvI64)(pTHX_ SV*);
-int       (*math_int64_c_api_SvI64OK)(pTHX_ SV*);
-uint64_t  (*math_int64_c_api_SvU64)(pTHX_ SV*);
-int       (*math_int64_c_api_SvU64OK)(pTHX_ SV*);
-SV *      (*math_int64_c_api_newSVi64)(pTHX_ int64_t);
-SV *      (*math_int64_c_api_newSVu64)(pTHX_ uint64_t);
-uint64_t  (*math_int64_c_api_randU64)(pTHX);
+#define fetch_ptr(to, name)                              \
+    svp = hv_fetchs(math_int64_capi_hash, name, 0);     \
+    if (!svp || !*svp) Perl_croak(aTHX_ "Unable to fetch pointer for " name " function"); \
+    to = INT2PTR(void *, SvIV(*svp))
 
-int
-perl_math_int64_load(int required_version) {
-    dTHX;
+void
+math_int64_boot(pTHX_ int version) {
+    dSP;
     SV **svp;
     eval_pv("require Math::Int64", TRUE);
-    if (SvTRUE(ERRSV)) return 0;
+    if (SvTRUE(ERRSV))
+        Perl_croak(aTHX_ "Unable to load Math::Int64: %s", SvPV_nolen(ERRSV));
 
-   math_int64_c_api_hash = get_hv("Math::Int64::C_API", 0);
-    if (!math_int64_c_api_hash) {
-        sv_setpv(ERRSV, "Unable to load Math::Int64 C API");
-        SvSETMAGIC(ERRSV);
-        return 0;
-    }
+    math_int64_capi_hash = get_hv("Math::Int64::C_API", 0);
+    if (!math_int64_capi_hash) Perl_croak(aTHX_ "Unable to load Math::Int64 C API");
 
-    svp = hv_fetch(math_int64_c_api_hash, "min_version", 11, 0);
-    if (!svp) svp = hv_fetch(math_int64_c_api_hash, "version", 7, 1);
-    if (!svp || !*svp) {
-        sv_setpv(ERRSV, "Unable to retrieve C API version for Math::Int64");
-        SvSETMAGIC(ERRSV);
-        return 0;
-    }
-    math_int64_c_api_min_version = SvIV(*svp);
+    math_int64_capi_version = SvIV(*hv_fetchs(math_int64_capi_hash, "version", 1));
+    if (math_int64_capi_version < version)
+        Perl_croak(aTHX_ "Math::Int64 C API version mismatch, expected %d, found %d",
+                   version, math_int64_capi_version);
 
-    svp = hv_fetch(math_int64_c_api_hash, "max_version", 11, 0);
-    if (!svp) svp = hv_fetch(math_int64_c_api_hash, "version", 7, 1);
-    if (!svp || !*svp) {
-        sv_setpv(ERRSV, "Unable to retrieve C API version for Math::Int64");
-        SvSETMAGIC(ERRSV);
-        return 0;
-    }
-    math_int64_c_api_max_version = SvIV(*svp);
-
-    if ((required_version < math_int64_c_api_min_version) ||
-        (required_version > math_int64_c_api_max_version)) {
-        sv_setpvf(ERRSV,
-                  "Math::Int64 C API version mismatch. "
-                  "The installed module supports versions %d to %d but %d is required",
-                  math_int64_c_api_min_version,
-                  math_int64_c_api_max_version,
-                  required_version);
-        SvSETMAGIC(ERRSV);
-        return 0;
-    }
-
-    svp = hv_fetch(math_int64_c_api_hash, "SvI64", 5, 0);
-    if (!svp || !*svp) {
-        sv_setpv(ERRSV, "Unable to fetch pointer 'SvI64' C function from Math::Int64");
-        SvSETMAGIC(ERRSV);
-        return 0;
-    }
-    math_int64_c_api_SvI64 = INT2PTR(void *, SvIV(*svp));
-    svp = hv_fetch(math_int64_c_api_hash, "SvI64OK", 7, 0);
-    if (!svp || !*svp) {
-        sv_setpv(ERRSV, "Unable to fetch pointer 'SvI64OK' C function from Math::Int64");
-        SvSETMAGIC(ERRSV);
-        return 0;
-    }
-    math_int64_c_api_SvI64OK = INT2PTR(void *, SvIV(*svp));
-    svp = hv_fetch(math_int64_c_api_hash, "SvU64", 5, 0);
-    if (!svp || !*svp) {
-        sv_setpv(ERRSV, "Unable to fetch pointer 'SvU64' C function from Math::Int64");
-        SvSETMAGIC(ERRSV);
-        return 0;
-    }
-    math_int64_c_api_SvU64 = INT2PTR(void *, SvIV(*svp));
-    svp = hv_fetch(math_int64_c_api_hash, "SvU64OK", 7, 0);
-    if (!svp || !*svp) {
-        sv_setpv(ERRSV, "Unable to fetch pointer 'SvU64OK' C function from Math::Int64");
-        SvSETMAGIC(ERRSV);
-        return 0;
-    }
-    math_int64_c_api_SvU64OK = INT2PTR(void *, SvIV(*svp));
-    svp = hv_fetch(math_int64_c_api_hash, "newSVi64", 8, 0);
-    if (!svp || !*svp) {
-        sv_setpv(ERRSV, "Unable to fetch pointer 'newSVi64' C function from Math::Int64");
-        SvSETMAGIC(ERRSV);
-        return 0;
-    }
-    math_int64_c_api_newSVi64 = INT2PTR(void *, SvIV(*svp));
-    svp = hv_fetch(math_int64_c_api_hash, "newSVu64", 8, 0);
-    if (!svp || !*svp) {
-        sv_setpv(ERRSV, "Unable to fetch pointer 'newSVu64' C function from Math::Int64");
-        SvSETMAGIC(ERRSV);
-        return 0;
-    }
-    math_int64_c_api_newSVu64 = INT2PTR(void *, SvIV(*svp));
-    svp = hv_fetch(math_int64_c_api_hash, "randU64", 7, 0);
-    if (!svp || !*svp) {
-        sv_setpv(ERRSV, "Unable to fetch pointer 'randU64' C function from Math::Int64");
-        SvSETMAGIC(ERRSV);
-        return 0;
-    }
-    math_int64_c_api_randU64 = INT2PTR(void *, SvIV(*svp));
-
-    return 1;
+    fetch_ptr(math_int64_capi_newSVi64, "newSVi64");
+    fetch_ptr(math_int64_capi_newSVu64, "newSVu64");
+    fetch_ptr(math_int64_capi_SvI64, "SvI64");
+    fetch_ptr(math_int64_capi_SvU64, "SvU64");
+    fetch_ptr(math_int64_capi_SvI64OK, "SvI64OK");
+    fetch_ptr(math_int64_capi_SvU64OK, "SvU64OK");
 }
--- trunk/libnf/perl/Net-NfDump/perl_math_int64.h
+++ trunk/libnf/perl/Net-NfDump/perl_math_int64.h
@@ -1,60 +1,51 @@
 /*
  * perl_math_int64.h - This file is in the public domain
- * Author: Salvador Fandino <sfandino@yahoo.com>
- * Version: 2.0
  *
- * Generated on: 2013-01-07 20:05:31
- * Math::Int64 version: 0.29
- * Module::CAPIMaker version: 0.02
+ * Author: Salvador Fandino <sfandino@yahoo.com>
+ * Version: 1.0
  */
 
 #if !defined (PERL_MATH_INT64_H_INCLUDED)
 #define PERL_MATH_INT64_H_INCLUDED
 
-#define MATH_INT64_C_API_REQUIRED_VERSION 2
-#define MATH_INT64_VERSION MATH_INT64_C_API_REQUIRED_VERSION
-
-int perl_math_int64_load(int required_version);
-
-#define PERL_MATH_INT64_LOAD perl_math_int64_load(MATH_INT64_C_API_REQUIRED_VERSION)
-#define PERL_MATH_INT64_LOAD_OR_CROAK \
-    if (PERL_MATH_INT64_LOAD);        \
-    else croak(NULL);
-#define MATH_INT64_BOOT PERL_MATH_INT64_LOAD_OR_CROAK
-
-extern HV *math_int64_c_api_hash;
-extern int math_int64_c_api_min_version;
-extern int math_int64_c_api_max_version;
-#define math_int64_capi_version math_int64_c_api_max_version
+#define MATH_INT64_VERSION 1
 
 #if (defined(MATH_INT64_NATIVE_IF_AVAILABLE) && (IVSIZE == 8))
 #define MATH_INT64_NATIVE 1
 #endif
 
-extern int64_t   (*math_int64_c_api_SvI64)(pTHX_ SV*);
-#define SvI64(a) ((*math_int64_c_api_SvI64)(aTHX_ (a)))
-extern int       (*math_int64_c_api_SvI64OK)(pTHX_ SV*);
-#define SvI64OK(a) ((*math_int64_c_api_SvI64OK)(aTHX_ (a)))
-extern uint64_t  (*math_int64_c_api_SvU64)(pTHX_ SV*);
-#define SvU64(a) ((*math_int64_c_api_SvU64)(aTHX_ (a)))
-extern int       (*math_int64_c_api_SvU64OK)(pTHX_ SV*);
-#define SvU64OK(a) ((*math_int64_c_api_SvU64OK)(aTHX_ (a)))
-extern SV *      (*math_int64_c_api_newSVi64)(pTHX_ int64_t);
-#define newSVi64(a) ((*math_int64_c_api_newSVi64)(aTHX_ (a)))
-extern SV *      (*math_int64_c_api_newSVu64)(pTHX_ uint64_t);
-#define newSVu64(a) ((*math_int64_c_api_newSVu64)(aTHX_ (a)))
-extern uint64_t  (*math_int64_c_api_randU64)(pTHX);
-#define randU64() ((*math_int64_c_api_randU64)(aTHX))
-
-
 #if MATH_INT64_NATIVE
 
-#undef newSVi64
+#define MATH_INT64_BOOT 
 #define newSVi64 newSViv
-#undef newSVu64
 #define newSVu64 newSVuv
+#define SvI64 SvIV
+#define SvU64 SvUV
+#define SvI64OK SvIOK
+#define SvU64OK SvIOK_UV
+
+#else
+
+extern HV *math_int64_capi_hash;                                       
+extern int math_int64_capi_version;
+extern SV *(*math_int64_capi_newSVi64)(pTHX_ int64_t);
+extern SV *(*math_int64_capi_newSVu64)(pTHX_ uint64_t);
+extern int64_t (*math_int64_capi_SvI64)(pTHX_ SV*);
+extern uint64_t (*math_int64_capi_SvU64)(pTHX_ SV*);
+extern int (*math_int64_capi_SvI64OK)(pTHX_ SV*);
+extern int (*math_int64_capi_SvU64OK)(pTHX_ SV*);
+
+void math_int64_boot(pTHX_ int version);
+
+#define MATH_INT64_BOOT math_int64_boot(aTHX_ MATH_INT64_VERSION)
+
+#define newSVi64(i64) (*math_int64_capi_newSVi64)(aTHX_ i64)
+#define newSVu64(u64) (*math_int64_capi_newSVu64)(aTHX_ u64)
+#define SvI64(sv) (*math_int64_capi_SvI64)(aTHX_ sv)
+#define SvU64(sv) (*math_int64_capi_SvU64)(aTHX_ sv)
+#define SvI64OK(sv) (*math_int64_capi_SvI64OK)(aTHX_ sv)
+#define SvU64OK(sv) (*math_int64_capi_SvU64OK)(aTHX_ sv)
 
 #endif
 
 #endif
-
