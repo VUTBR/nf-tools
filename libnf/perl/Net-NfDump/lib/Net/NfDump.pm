8ecc67183c6951a38e880eb731406ff610a2f9f3
tpoder@cis.vutbr.cz <tpoder1@users.noreply.github.com>
2013-04-21 07:07:59 +0000

    Text update

--- libnf/perl/Net-NfDump/lib/Net/NfDump.pm
+++ libnf/perl/Net-NfDump/lib/Net/NfDump.pm
@@ -13,6 +13,7 @@ use Net::NfDump::Fields;
 
 our @ISA = qw(Exporter);
 
+# XXX
 # Items to export into callers namespace by default. Note: do not export
 # names by default without a very good reason. Use EXPORT_OK instead.
 # Do not simply export all your public functions/methods/constants.
