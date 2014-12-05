fd8b8637e757c7520d99e104a551adb4c8651e60
tpoder@cis.vutbr.cz <tpoder@cis.vutbr.cz@6e27440e-4b98-2107-0108-1f70ee4755a6>
2014-12-05 21:21:07 +0000

    prepared for distribution

--- trunk/libnf/perl/Net-NfDump/testdist.sh
+++ trunk/libnf/perl/Net-NfDump/testdist.sh
@@ -1,10 +1,13 @@
 #!/bin/bash 
 
 HOSTS="root@hawk.cis.vutbr.cz root@coyote.cis.vutbr.cz tpoder@185.62.109.9 test-ubuntu"
-DIST="Net-NfDump-1.04"
+DIST="Net-NfDump-1.05"
 EXT=".tar.gz"
 
 for h in $HOSTS; do 
+	echo ***********************************************
+	echo ${h}
+	echo ***********************************************
 	scp ${DIST}${EXT} ${h}:/tmp/
 	ssh ${h} "export AUTOMATED_TESTING=1 ; cd /tmp/ && (rm -rf ${DIST}/ ; tar xzf ${DIST}${EXT}) && cd ${DIST} && perl Makefile.PL && make test ; rm -rf Net-Dump-*"
 
