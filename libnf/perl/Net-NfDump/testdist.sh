52063fb5cd71675eee1239c47bc09e00c1e20612
tpoder@cis.vutbr.cz <tpoder1@users.noreply.github.com>
2014-12-06 15:56:30 +0000

    prepared for new release

--- libnf/perl/Net-NfDump/testdist.sh
+++ libnf/perl/Net-NfDump/testdist.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
-HOSTS="root@hawk.cis.vutbr.cz root@coyote.cis.vutbr.cz tpoder@185.62.109.9 test-ubuntu"
-DIST="Net-NfDump-1.05"
+HOSTS="root@hawk.cis.vutbr.cz root@coyote.cis.vutbr.cz tpoder@185.62.109.9 root@test-ubuntu.net.vutbr.cz root@test-freebsd.net.vutbr.cz"
+DIST="Net-NfDump-1.07"
 EXT=".tar.gz"
 
 for h in $HOSTS; do 
@@ -9,7 +9,7 @@ for h in $HOSTS; do
 	echo ${h}
 	echo "***********************************************"
 	scp ${DIST}${EXT} ${h}:/tmp/
-	ssh ${h} "export AUTOMATED_TESTING=1 ; cd /tmp/ && (rm -rf ${DIST}/ ; tar xzf ${DIST}${EXT}) && cd ${DIST} && perl Makefile.PL && make test ; rm -rf Net-Dump-*"
+	ssh ${h} "export AUTOMATED_TESTING=1 ; cd /tmp/ && (rm -rf ${DIST}/ ; tar xzf ${DIST}${EXT}) && cd ${DIST} && perl Makefile.PL && make -j 8 -j 8 && make test ; rm -rf Net-Dump-*"
 
 done
 
