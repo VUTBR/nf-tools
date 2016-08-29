e3fe6e54f4d42ebf5eb330455b4a10128fc4450e
Imrich Štoffa <xstoff02@stud.fit.vutbr.cz>
2016-08-29 15:19:38 +0200

    Changes synced from ipfixcol. Added comments Convertor and eval for timestamp type Added substring search for string type as default operator Various bugfixes

--- libnf/c/src/ffilter/ffilter.c
+++ libnf/c/src/ffilter/ffilter.c
@@ -25,6 +25,8 @@
 #include "ffilter_gram.h"
 #include "ffilter.h"
 
+#define ff_reint_cast(type, ptr) (*(((type)*)(ptr)))
+
 /**
  * \brief Convert unit character to positive power of 10
  * \param[in] unit Suffix of number
@@ -58,12 +60,13 @@ int64_t get_unit(char *unit)
  * \param[in] valstr Literal number
  * \param endptr Place to store an address where conversion finised
  */
-uint64_t strtoul_unit(char *valstr, char**endptr)
+uint64_t strtoull_unit(char *valstr, char**endptr)
 {
 	uint64_t tmp64;
 	uint64_t mult = 0;
 
-	tmp64 = strtoul(valstr, endptr, 10);
+	//Base 0 - given the string 0x is base 16 0x0 is 8 and no prefix is base 10
+	tmp64 = strtoull(valstr, endptr, 0);
 	if (!**endptr) {
 		return tmp64;
 	}
@@ -84,7 +87,8 @@ int64_t strtoll_unit(char *valstr, char**endptr)
 	int64_t tmp64;
 	int64_t mult = 0;
 
-	tmp64 = strtoll(valstr, endptr, 10);
+	//Base 0 - given the string 0x is base 16 0x0 is 8 and no prefix is base 10
+	tmp64 = strtoll(valstr, endptr, 0);
 	if (!**endptr) {
 		return tmp64;
 	}
@@ -102,13 +106,13 @@ int64_t strtoll_unit(char *valstr, char**endptr)
 
 /* convert string into uint64_t */
 /* also converts string with units (64k -> 64000) */
-int str_to_uint(char *str, int type, char **res, size_t *vsize)
+int str_to_uint(ff_t *filter, char *str, ff_type_t type, char **res, size_t *vsize)
 {
 	uint64_t tmp64;
 	void *ptr;
 
 	char* endptr;
-	tmp64 = strtoul_unit(str, &endptr);
+	tmp64 = strtoull_unit(str, &endptr);
 	if (*endptr){
 		return 1;
 	}
@@ -147,7 +151,7 @@ int str_to_uint(char *str, int type, char **res, size_t *vsize)
 
 /* convert string into int64_t */
 /* also converts string with units (64k -> 64000) */
-int str_to_int(char *str, int type, char **res, size_t *vsize)
+int str_to_int(ff_t *filter, char *str, ff_type_t type, char **res, size_t *vsize)
 {
 
 	int64_t tmp64;
@@ -191,11 +195,64 @@ int str_to_int(char *str, int type, char **res, size_t *vsize)
 	return 0;
 }
 
+int str_to_uint64(ff_t *filter, char *str, char **res, size_t *vsize)
+{
+	return str_to_uint(filter, str, FF_TYPE_UINT64, res, vsize);
+}
+
+int str_to_int64(ff_t *filter, char *str, char **res, size_t *vsize)
+{
+	return str_to_int(filter, str, FF_TYPE_INT64, res, vsize);
+}
+
+/**
+ *
+ * \param filter
+ * \param str
+ * \param res
+ * \param vsize
+ * \return
+ */
+int str_to_real(ff_t *filter, char *str, char **res, size_t *vsize)
+{
+	double tmp64;
+	void *ptr;
+
+	char *endptr;
+	tmp64 = strtod(str, &endptr);
+
+	if (*endptr){
+		return 1;
+	}
+
+	*vsize = sizeof(double);
+
+	ptr = malloc(*vsize);
+
+	if (ptr == NULL) {
+		return 1;
+	}
+
+	memcpy(ptr, &tmp64, *vsize);
+
+	*res = ptr;
+
+	return 0;
+}
+
+/**
+ * \brief Transform mask representation
+ * from number of network bits format to full bit-mask
+ * \param numbits Number of network portion bits
+ * \param mask Ip address containing full mask
+ * \return Zero on succe
+ */
 int int_to_netmask(int *numbits, ff_ip_t *mask)
 {
 	int req_oct;
 	int retval = 0;
-	if (*numbits > 128) { *numbits = 128; retval = 1;}
+	if (*numbits > 128 || *numbits < 0) { *numbits = 128; retval = 1;}
+	//if (*numbits == 0) { retval = 1;}
 
 	//req_oct = (*numbits >> 5) + ((*numbits & 0b11111) > 0); //Get number of reqired octets
 
@@ -210,6 +267,12 @@ int int_to_netmask(int *numbits, ff_ip_t *mask)
 	return retval;
 }
 
+/**
+ * \brief Pad necessary zeros to shortened ipv4 address
+ * \param ip_str Shortened ip string
+ * \param numbits Number of network portion bits
+ * \return Autocompleted network address in new string (must be freed) \see strdup
+ */
 char* unwrap_ip(char *ip_str, int numbits)
 {
 	char *endptr = ip_str;
@@ -240,14 +303,25 @@ char* unwrap_ip(char *ip_str, int numbits)
 }
 
 /* convert string into ff_addr_t */
-int str_to_addr(ff_t *filter, char *str, char **res, int *numbits, size_t *size)
+/**
+ * \brief Extended conversion from ip string to internal representation
+ * \param filter
+ * \param str
+ * \param res
+ * \param numbits
+ * \param size
+ * \return
+ */
+int str_to_addr(ff_t *filter, char *str, char **res, size_t *size)
 {
 	ff_net_t *ptr;
 	char *saveptr;
 	char *ip_str = strdup(str);
 	char *ip;
 	char *mask;
-	int ip_ver = 4; //Guess ip version
+	int ip_ver = 0; //Guess ip version
+
+	int numbits;
 
 	ptr = malloc(sizeof(ff_net_t));
 
@@ -256,7 +330,7 @@ int str_to_addr(ff_t *filter, char *str, char **res, int *numbits, size_t *size)
 	}
 
 	memset(ptr, 0x0, sizeof(ff_net_t));
-	*numbits = 0;
+	numbits = 0;	//Not specified
 	*res = (char *)ptr;
 
 	ip = strtok_r(ip_str, "\\/ ", &saveptr);
@@ -267,12 +341,13 @@ int str_to_addr(ff_t *filter, char *str, char **res, int *numbits, size_t *size)
 		memset(&ptr->mask, ~0, sizeof(ff_ip_t));
 
 	} else {
-		*numbits = strtoul(mask, &saveptr, 10);
+		numbits = strtoul(mask, &saveptr, 10);
 
 		// Conversion does not end after first number maybe full mask was given
 		if (*saveptr) {
-			if (inet_pton(AF_INET, mask, &(ptr->mask.data[3]))) {
-				;
+			numbits = 0;
+			if (inet_pton(AF_INET, mask, &(ptr->mask.data[0]))) {
+				ip_ver = 4;
 			} else if (inet_pton(AF_INET6, mask, &ptr->mask.data)) {
 				ip_ver = 6;
 			} else {
@@ -283,24 +358,31 @@ int str_to_addr(ff_t *filter, char *str, char **res, int *numbits, size_t *size)
 			}
 		} else {
 			//for ip v6 require ::0 if address is shortened;
-			int_to_netmask(numbits, &(ptr->mask));
+			if (int_to_netmask(&numbits, &(ptr->mask))) {
+				free(ptr);
+				free(ip_str);
+				return 1;
+			}
 			//Try to unwrap ipv4 address
-			ip = unwrap_ip(ip_str, *numbits);
+			ip = unwrap_ip(ip_str, numbits);
 			if (ip) {
+				ip_ver = 4;
 				free(ip_str);
 				ip_str = ip;
+			} else {
+				ip_ver = 6;
 			}
 		}
 	}
 
-	if (inet_pton(AF_INET, ip_str, &(ptr->ip.data[3]))) {
+	if (inet_pton(AF_INET, ip_str, &(ptr->ip.data[3])) && (numbits <= 32) && ip_ver != 6 ) {
 		ptr->mask.data[3] = ptr->mask.data[0];
 		ptr->mask.data[0] = 0;
 		ptr->mask.data[1] = 0;
 		ptr->mask.data[2] = 0;
-		ip_ver = 4;
-	} else if (inet_pton(AF_INET6, ip_str, &ptr->ip)) {
-		ip_ver = 6;
+		ptr->ver = 4;
+	} else if (inet_pton(AF_INET6, ip_str, &ptr->ip) && (ip_ver != 4)) {
+		ptr->ver = 6;
 	} else {
 		free(ptr);
 		free(ip_str);
@@ -313,7 +395,6 @@ int str_to_addr(ff_t *filter, char *str, char **res, int *numbits, size_t *size)
 
 	free(ip_str);
 
-	*numbits = ip_ver == 4 ? 32 : 128;
 	*res = &(ptr->ip);
 
 	*size = sizeof(ff_net_t);
@@ -328,7 +409,7 @@ int str_to_addr(ff_t *filter, char *str, char **res, int *numbits, size_t *size)
  * \param size Number of bits allocated
  * \return Zero on failure
  */
-int str_to_mac(char *str, char **res, size_t *size)
+int str_to_mac(ff_t *filter, char *str, char **res, size_t *size)
 {
 	char *ptr;
 
@@ -339,15 +420,14 @@ int str_to_mac(char *str, char **res, size_t *size)
 
 	char *endptr = str;
 
-	int ret = 0;
+	int ret = 1;
 	uint32_t num = 0;
 	for (int x = 0; x < 6; x++) {
 		num = strtoul(endptr, &endptr, 16);
 		if (num > 255) {
-			ret = 1;
 			break;
 		}
-		(ptr)[x] = num;
+		((char *)ptr)[x] = num;
 
 		while (isspace(*endptr)) {
 			endptr++;
@@ -355,20 +435,19 @@ int str_to_mac(char *str, char **res, size_t *size)
 
 		if (*endptr == ':') {
 			endptr++;
-		} else if (isxdigit(*endptr)) { ;
+			while (isspace(*endptr)) {
+				endptr++;
+			}
+		}
+		if (isxdigit(*endptr)) { ;
 		} else if (x == 5 && !*endptr) {
 			ret = 0;
 		} else {
-			ret = 1;
 			break;
-		}
-
-		while (isspace(*endptr)) {
-			endptr++;
 		}
 	}
 	if (ret) {
-		free(*ptr);
+		free(ptr);
 		*size = 0;
 	} else {
 		*res = ptr;
@@ -377,21 +456,27 @@ int str_to_mac(char *str, char **res, size_t *size)
 	return ret;
 }
 
-
-int str_to_timestamp(char* str, char** res, size_t *size)
+int str_to_timestamp(ff_t *filter, char* str, char** res, size_t *size)
 {
 	struct tm tm;
-	time_t timest;
+	ff_timestamp_t timest;
 
-	if (strptime(str, "%Y-%m-%d %H:%M:%S", &tm) == NULL) { return 1; }
+	if (strptime(str, "%F%n%T", &tm) == NULL) {
+		return 1;
+	}
+
 	timest = mktime(&tm);
 
-	char *ptr = malloc(sizeof(uint64_t));
-	if (!ptr) return 1;
+	char *ptr = malloc(sizeof(ff_timestamp_t));
+	if (!ptr) {
+		return 1;
+	}
 
-	*ptr = timest;
-	memcpy(*res, ptr, sizeof(uint64_t));
-	*size = sizeof(uint64_t);
+	timest *= 1000;
+
+	memcpy(ptr, &timest, sizeof(ff_timestamp_t));
+	*res = ptr;
+	*size = sizeof(ff_timestamp_t);
 
 	return 0;
 }
@@ -404,7 +489,7 @@ ff_error_t ff_type_cast(yyscan_t *scanner, ff_t *filter, char *valstr, ff_node_t
 	case FF_TYPE_UINT32:
 	case FF_TYPE_UINT16:
 	case FF_TYPE_UINT8:
-		if (str_to_uint(valstr, node->type, &node->value, &node->vsize)) {
+		if (str_to_uint(filter, valstr, node->type, &node->value, &node->vsize)) {
 			ff_set_error(filter, "Can't convert '%s' into numeric value", valstr);
 			return FF_ERR_OTHER_MSG;
 		}
@@ -413,13 +498,21 @@ ff_error_t ff_type_cast(yyscan_t *scanner, ff_t *filter, char *valstr, ff_node_t
 	case FF_TYPE_INT32:
 	case FF_TYPE_INT16:
 	case FF_TYPE_INT8:
-		if (str_to_int(valstr, node->type, &node->value, &node->vsize)) {
+		if (str_to_int(filter, valstr, node->type, &node->value, &node->vsize)) {
 			ff_set_error(filter, "Can't convert '%s' into numeric value", valstr);
 			return FF_ERR_OTHER_MSG;
 		}
 		break;
+
+	case FF_TYPE_DOUBLE:
+		if (str_to_real(filter, valstr, &node->value, &node->vsize)) {
+			ff_set_error(filter, "Can't convert '%s' to real number", valstr);
+			return FF_ERR_OTHER_MSG;
+		}
+		break;
+
 	case FF_TYPE_ADDR:
-		if (str_to_addr(filter, valstr, &node->value, &node->numbits, &node->vsize)) {
+		if (str_to_addr(filter, valstr, &node->value, &node->vsize)) {
 			ff_set_error(filter, "Can't convert '%s' into IP address", valstr);
 			return FF_ERR_OTHER_MSG;
 		}
@@ -428,7 +521,7 @@ ff_error_t ff_type_cast(yyscan_t *scanner, ff_t *filter, char *valstr, ff_node_t
 		// unsigned with undefined data size (internally mapped to uint64_t in network order) */
 	case FF_TYPE_UNSIGNED_BIG:
 	case FF_TYPE_UNSIGNED:
-		if (str_to_uint(valstr, FF_TYPE_UINT64, &node->value, &node->vsize)) {
+		if (str_to_uint64(filter, valstr, &node->value, &node->vsize)) {
 			node->value = calloc(1, sizeof(uint64_t));
 			if (!node->value) return FF_ERR_NOMEM;
 			node->vsize = sizeof(uint64_t);
@@ -447,7 +540,7 @@ ff_error_t ff_type_cast(yyscan_t *scanner, ff_t *filter, char *valstr, ff_node_t
 		break;
 	case FF_TYPE_SIGNED_BIG:
 	case FF_TYPE_SIGNED:
-		if (str_to_int(valstr, FF_TYPE_INT64, &node->value, &node->vsize)) {
+		if (str_to_int64(filter, valstr, &node->value, &node->vsize)) {
 			node->value = calloc(1, sizeof(uint64_t));
 			node->vsize = sizeof(uint64_t);
 			if (!node->value) return FF_ERR_NOMEM;
@@ -472,16 +565,19 @@ ff_error_t ff_type_cast(yyscan_t *scanner, ff_t *filter, char *valstr, ff_node_t
 		node->vsize = strlen(valstr);
 		break;
 	case FF_TYPE_MAC:
-		if (str_to_mac(valstr, &node->value, &node->vsize)) {
+		if (str_to_mac(filter, valstr, &node->value, &node->vsize)) {
 			ff_set_error(filter, "Can't convert '%s' into mac address", valstr);
 			return FF_ERR_OTHER_MSG;
 		}
 		break;
+
+	case FF_TYPE_TIMESTAMP_BIG:
 	case FF_TYPE_TIMESTAMP:
-		if (str_to_timestamp(valstr, &node->value, &node->vsize)) {
-			ff_set_error(filter, "Can't convert '%s' to time", valstr);
+		if (str_to_timestamp(filter, valstr, &node->value, &node->vsize)) {
+			ff_set_error(filter, "Can't convert '%s' to timestamp", valstr);
 			return FF_ERR_OTHER_MSG;
 		}
+		break;
 	default:
 		ff_set_error(filter, "Can't convert '%s' type is unsupported", valstr);
 		return FF_ERR_OTHER_MSG;
@@ -509,13 +605,14 @@ const char* ff_error(ff_t *filter, const char *buf, int buflen) {
 }
 
 /**
- * \brief Create structure of nodes necessary to save list nodes for each ff_external_id
- * \param[in] Node - list used as template
+ * \brief Build node tree with leaf nodes for each ff_external_id
+ * \param[in] Node - leaf used as template
  * \param oper - FF_OP_AND or FF_OP_OR defines how structure will evaluate \see ff_oper_t
- * \param lvalue - Information about information element
- * \return root node of new subtree or NULL on error
+ * \param[in] lvalue - Info about field
+ * \return Root node of new subtree or NULL on error
  */
 ff_node_t* ff_branch_node(ff_node_t *node, ff_oper_t oper, ff_lvalue_t* lvalue) {
+
 	ff_node_t *dup[FF_MULTINODE_MAX] = {0};
 	int err = 0;
 	int x = 0;
@@ -612,7 +709,9 @@ ff_node_t* ff_new_leaf(yyscan_t scanner, ff_t *filter, char *fieldstr, ff_oper_t
 
 			/* Change evaluation operator when no operator was specified */
 		if (oper == FF_OP_NOOP) {
-			if (lvalue.options & FFOPTS_FLAGS) {
+			if (lvalue.options & FF_OPTS_FLAGS) {
+				oper = FF_OP_ISSET;
+			} else if (lvalue.type == FF_TYPE_STRING ) {
 				oper = FF_OP_ISSET;
 			} else {
 				oper = FF_OP_EQ;
@@ -666,7 +765,7 @@ ff_node_t* ff_new_leaf(yyscan_t scanner, ff_t *filter, char *fieldstr, ff_oper_t
 		node->left = NULL;
 		node->right = NULL;
 
-		if (!(lvalue.options & FFOPTS_MULTINODE)) {
+		if (!(lvalue.options & FF_OPTS_MULTINODE)) {
 			if (multinode) {
 				ff_free_node(node);
 				retval = NULL;
@@ -710,9 +809,13 @@ ff_node_t* ff_new_node(yyscan_t scanner, ff_t *filter, ff_node_t* left, ff_oper_
 	return node;
 }
 
+//TODO Refactor this to better readable form. Maybe inlined functions to eval each type and convert _BIG variant to flag
+//TODO: Add for timestamp types, check possible conflicts in node and data types
+//Big suffix refers to what endiannes to expect from data function, note that comparation uses native format of architecture
 int ff_oper_eval(char* buf, size_t size, ff_node_t *node)
 {
 	int res = 0;
+	int x = 0;
 	switch (node->oper) {
 
 	case FF_OP_EQ:
@@ -729,7 +832,9 @@ int ff_oper_eval(char* buf, size_t size, ff_node_t *node)
 
 		case FF_TYPE_DOUBLE: return *(double *) buf == *(double *) node->value;
 		case FF_TYPE_STRING: return !strcmp((char *) buf, node->value);
+		case FF_TYPE_MAC: return !memcmp(buf, node->value, sizeof(ff_mac_t));
 
+		case FF_TYPE_TIMESTAMP_BIG:
 		case FF_TYPE_UNSIGNED_BIG:
 			if (size > node->vsize) { return -1; }                /* too big integer */
 			switch (size) {
@@ -739,6 +844,7 @@ int ff_oper_eval(char* buf, size_t size, ff_node_t *node)
 			case sizeof(uint64_t): return ntohll(*(uint64_t *) buf) == *(uint64_t *)node->value;
 			default: return -1;
 			}
+		case FF_TYPE_TIMESTAMP:
 		case FF_TYPE_UNSIGNED:
 			if (size > node->vsize) { return -1; }                /* too big integer */
 			switch (size) {
@@ -768,24 +874,36 @@ int ff_oper_eval(char* buf, size_t size, ff_node_t *node)
 			}
 		case FF_TYPE_ADDR:
 			/* Compare masked ip addresses */
+			//TODO What if i get ipv4 in ff_addr_t structure
 			switch (size) {
+			//Comparation of v4 to v6 is forbidden
 			case sizeof(ff_ip_t):
-				if (node->numbits != 128) { return 0; }
-
+				x = 0;
+				// If mask in node indicates that address will be ipv4 skip ipv6 portion
+				// But what if ip version in buf does not match with node
+				if (((ff_net_t *) node->value)->ver == 4) { //Node is v4
+					if (((ff_ip_t *) buf)->data[0] || ((ff_ip_t *) buf)->data[1] || ((ff_ip_t *) buf)->data[2]) {
+						//And data v6
+						return -1;
+					}
+					// Skip v6 portion
+					x = 3;
+				}
 				res = 1;
-				for (int x = 0; x < 4; x++) {
-					res = res && (((*(ff_ip_t *) buf).data[x] &
+				for ( ; x < 4; x++) {
+					res = res && ((((ff_net_t *) buf)->ip.data[x] &
 								   ((ff_net_t *) node->value)->mask.data[x]) ==
-								  ((ff_net_t *) node->value)->ip.data[x]);
+								   ((ff_net_t *) node->value)->ip.data[x]);
 				}
 				return res;
+			// Data are shortened
 			case sizeof(uint32_t):
-				/* Eval fails if no bits are compared */
-				if (node->numbits != 32) { return 0; }
+				if (((ff_net_t *) node->value)->ver != 4) { return -1; }
+				res = (((*(uint32_t *) buf) &
+					   ((ff_net_t *) node->value)->mask.data[3]) ==
+					   ((ff_net_t *) node->value)->ip.data[3]);
 
-				return (((*(uint32_t *) buf) &
-						 ((ff_net_t *) node->value)->mask.data[3]) ==
-						((ff_net_t *) node->value)->ip.data[3]);
+				return res;
 			default: return -1;
 			}
 		default: return -1;
@@ -806,6 +924,7 @@ int ff_oper_eval(char* buf, size_t size, ff_node_t *node)
 		case FF_TYPE_DOUBLE: return *(double *) buf > *(double *) node->value;
 		case FF_TYPE_STRING: return strcmp((char *) buf, node->value) > 0;
 
+		case FF_TYPE_TIMESTAMP_BIG:
 		case FF_TYPE_UNSIGNED_BIG:
 			if (size > node->vsize) { return -1; }                /* too big integer */
 			switch (size) {
@@ -815,6 +934,8 @@ int ff_oper_eval(char* buf, size_t size, ff_node_t *node)
 			case sizeof(uint64_t): return ntohll(*(uint64_t *) buf) > *(uint64_t *)node->value;
 			default: return -1;
 			}
+
+		case FF_TYPE_TIMESTAMP:
 		case FF_TYPE_UNSIGNED:
 			if (size > node->vsize) { return -1; }                /* too big integer */
 			switch (size) {
@@ -860,6 +981,7 @@ int ff_oper_eval(char* buf, size_t size, ff_node_t *node)
 		case FF_TYPE_DOUBLE: return *(double *) buf < *(double *) node->value;
 		case FF_TYPE_STRING: return strcmp((char *) buf, node->value) < 0;
 
+		case FF_TYPE_TIMESTAMP_BIG:
 		case FF_TYPE_UNSIGNED_BIG:
 			if (size > node->vsize) { return -1; }                /* too big integer */
 			switch (size) {
@@ -869,6 +991,8 @@ int ff_oper_eval(char* buf, size_t size, ff_node_t *node)
 			case sizeof(uint64_t): return ntohll(*(uint64_t *) buf) < *(uint64_t *)node->value;
 			default: return -1;
 			}
+
+		case FF_TYPE_TIMESTAMP:
 		case FF_TYPE_UNSIGNED:
 			if (size > node->vsize) { return -1; }                /* too big integer */
 			switch (size) {
@@ -937,11 +1061,15 @@ int ff_oper_eval(char* buf, size_t size, ff_node_t *node)
 					   *(uint8_t *) node->value;
 			default: return -1;
 			}
+
+		case FF_TYPE_STRING: return (strcasestr(buf, node->value) != NULL);
+
 		default: return -1;
 		}
 
 	case FF_OP_EXIST:
-		return 1;
+		if (size > 0) return 1;
+		return 0;
 
 	default: return -1;
 	}
@@ -1011,8 +1139,14 @@ int ff_eval_node(ff_t *filter, ff_node_t *node, void *rec) {
 	if (filter->options.ff_data_func(filter, rec, node->field, buf, &size) != FF_OK) {
 		//ff_set_error(filter, "Can't get data");
 		//On no data mimic zero
-		size = sizeof(int64_t);
-		((int64_t *)buf)[0] = 0L;
+		switch (node->type) {
+		case FF_TYPE_MAC: size = sizeof(ff_mac_t); break;
+		case FF_TYPE_ADDR: size = sizeof(ff_ip_t); break;
+		case FF_TYPE_DOUBLE: size = sizeof(ff_double_t); break;
+		case FF_TYPE_TIMESTAMP: size = sizeof(ff_timestamp_t); break;
+		default: size = node->vsize;
+		}
+		memset(buf, 0, size);
 		exist = 0;	// No data found
 	}
 
